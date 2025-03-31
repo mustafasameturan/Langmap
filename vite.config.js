deea729601f32e367602b39338e4e4a7c46f42cb
mustafasameturan <mustafasameturan@gmail.com>
2025-04-01 02:41:29 +0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    cors: true,
-    strictPort: true,
-  },
-  preview: {
-    host: true,
-    port: 4173,
-    strictPort: true,
-    cors: true,
-    allowedHosts: [
-      'langmap.server-mst.com.tr',
-      'server-mst.com.tr',
-      'localhost'
-    ]
-  }
+  allowedHosts: ['langmap.server-mst.com.tr']
 })
