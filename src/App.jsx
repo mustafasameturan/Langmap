67bed677cc7f0c952ebb310a200c01bf3adda37c
mustafasameturan <mustafasameturan@gmail.com>
2025-04-01 02:23:52 +0300

    Update Vite configuration to allow specific preview hosts and remove debug log from App component

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,6 @@ function App() {
     }
   }, [])
 
-  console.log("test");
-
   // Dark mode değiştiğinde localStorage'a kaydet ve HTML sınıfını güncelle
   useEffect(() => {
     localStorage.setItem('darkMode', darkMode)
