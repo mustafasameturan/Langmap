d971551fbf9408e930167e6869e955eca8916cf7
mustafasameturan <mustafasameturan@gmail.com>
2025-04-01 16:03:59 +0300

    Update Vite configuration to enhance CORS settings and refine allowed hosts for server

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-	preview: {
-		port: 4173,
-		strictPort: true,
-		host: '0.0.0.0', 
-		allowedHosts: true,
-	},
 	server: {
-		allowedHosts: true
+		cors: {
+			origin: ['https://langmap.server-mst.com.tr', 'http://localhost:4173'],
+			methods: ['GET', 'POST'],
+			allowedHeaders: ['Content-Type']
+		},
+		allowedHosts: ['langmap.server-mst.com.tr', '*.server-mst.com.tr', 'localhost', '*']
 	}
 })
